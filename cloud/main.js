b6d6025f62153952cddb1fc2bc86d3e520a94ad2
Honghao Zhang <zhh358@gmail.com>
2014-06-09 19:55:36 -0400

    add original 'hello world' function

--- cloud/main.js
+++ cloud/main.js
@@ -1,33 +1,39 @@
 // Check whether an object is a number
 function isNumber(n) {
-	return !isNaN(parseFloat(n)) && isFinite(n);
+    return !isNaN(parseFloat(n)) && isFinite(n);
 }
-
+ 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("forwardAPI", function(request, response) {
-
-	sid = request.params.sid;
-	if (!isNumber(sid)){
-		errmsg = "Sid is not a valid number, got: ".concat(sid);
-		console.error(errmsg);
-		response.error(errmsg);
-		return;
-	}
-
-	Parse.Cloud.httpRequest({
+ 
+    sid = request.params.sid;
+    if (!isNumber(sid)){
+        errmsg = "Sid is not a valid number, got: ".concat(sid);
+        console.error(errmsg);
+        response.error(errmsg);
+        return;
+    }
+ 
+    Parse.Cloud.httpRequest({
   method: "POST",
   url: "http://services.tvrage.com/feeds/episode_list.php",
   params: {
      sid:sid
   },
-	success: function(httpResponse) {
+    success: function(httpResponse) {
     console.log('Request successful' + httpResponse.status);
     response.success(httpResponse.text);
   },
   error: function(httpResponse) {
     console.error('Request failed with response code ' + httpResponse.status);
   }
-	});
+    });
   //response.success("Hello world!");
+});
+
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.Cloud.define("hello", function(request, response) {
+  response.success("Hello world!");
 });
