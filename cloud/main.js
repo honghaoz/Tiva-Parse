4a13609943e0867177f7f1055dd8272192b65dde
Honghao Zhang <zhh358@gmail.com>
2014-06-12 13:27:32 -0400

    added ratings

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,6 @@
+
+var ratingsModule = require('cloud/Ratings.js');
+
 // Check whether an object is a number
 function isNumber(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
@@ -38,4 +41,19 @@ var myModule = require('cloud/api_experiment.js');
 // For example:
 Parse.Cloud.define("testHTTP", function(request, response) {
   myModule.queryMonth("2014Jun", request, response);
+});
+
+Parse.Cloud.define("testRating", function(request, response) {
+  var aRating = new Ratings(95, 8, 8, 0);
+  aRating.save(null, {
+    success: function(aRating) {
+      // Execute any logic that should take place after the object is saved.
+      alert('New Ratings created with percentage: ' + aRating.percentage);
+    },
+    error: function(aRating, error) {
+      // Execute any logic that should take place if the save fails.
+      // error is a Parse.Error with an error code and description.
+      alert('Failed to create new object, with error code: ' + error.description);
+    }
+  });
 });
