ff31517c831d661eb125132e18c3d25e1d1f536d
Alfred Chan <a77chan@uwaterloo.ca>
2014-06-18 23:51:58 -0400

    new daily job for updating API data

--- cloud/Show.js
+++ cloud/Show.js
@@ -1,43 +1,83 @@
 var Show = Parse.Object.extend("Show", {
   // Instance methods
-  initialize: function (attrs, options) {
-  }
-}, {
+	initialize: function (attrs, options) {}
+	}, {
   // Class methods
-  create: function(show, episode) {
+  create: function(show, status) {
     var entry = new Show();
+
+		entry.set( {
+			"Title": show.title,
+			"Year": show.year * 1,
+			"Country": show.country,
+			"Overview": show.overview,
+			"Runtime": show.runtime * 1,
+			"Network": show.network,
+			"Genres" : show.genres,
+			"tvdb_id": show.tvdb_id * 1,
+			"Poster": show.images.poster,
+			"Fanart": show.images.fanart,
+			"Banner": show.images.banner
+		});
     return entry;
   }
 });
 
-function processSingleEpisode(show, episode) {
-	var buffer = "";
-	for (var prop in show) buffer += prop + "="+show[prop] + "        ";
-	return buffer;
-}
+var Episode = Parse.Object.extend("Episode", {
+  // Instance methods
+	initialize: function (attrs, options) {}
+	}, {
+  // Class methods
+  create: function(showObj, episode) {
+    var entry = new Episode();
 
-function processJsonData(jsonData) {
-	var buffer = "";
+		entry.set( {
+			"Season": episode.season * 1,
+			"Number": episode.number * 1,
+			"Title": episode.title,
+			"Overview": episode.overview,
+			"Screen": episode.images.screen,
+			"Air_Date_UTC": new Date (episode.first_aired_utc * 1000),
+			"Parent": showObj,
+		});
+    return entry;
+  }
+});
+
+function processJsonData(jsonData, status) {
+	var allObjects = [];
 	for (var i = 0; i < jsonData.length; i++){
 		var allEpisodeData = jsonData[i]["episodes"];
 		for (var j = 0; j < allEpisodeData.length; j++) {
-			buffer += processSingleEpisode(allEpisodeData[j].show, allEpisodeData[j].data);
+			var curShow = Show.create(allEpisodeData[j].show, status);
+			var curEpisode = Episode.create(curShow, allEpisodeData[j].episode);
+
+			// storing Episode objects automatically store its parent (i.e. Show)
+			allObjects.push(curEpisode);
 		}
 	}
-	return buffer;
+	Show.saveAll(allObjects, {
+		success: function(list) {
+			status.success("OKAY");
+		},
+		error: function(error) {
+			status.error("FAILURE");
+		}
+	});
 }
 
-Parse.Cloud.define("updateTomorrowData", function (request, response) {
+Parse.Cloud.job("updateTomorrowData", function (request, status) {
 
 	Parse.Cloud.httpRequest({
 		method: "GET",
 		url: "http://api.trakt.tv/calendar/shows.json/1113cff76935521fb75e4bd9336e9a4c/tomorrow/1",
 		success: function (httpResponse) {
-			console.log('Request successful' + httpResponse.status);
-			response.success(processJsonData(JSON.parse(httpResponse.text)));
+
+			console.log("Request from API successful" + httpResponse.status);
+			processJsonData(JSON.parse(httpResponse.text), status);
 		},
 		error: function (httpResponse) {
-			console.error('Request failed with response code ' + httpResponse.status);
+			console.error("Request failed with response code " + httpResponse.status);
 		}
 	});
 });
--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,3 @@
 // Include modeules
 var ratingModule = require('cloud/Rating.js');
 var showModule = require('cloud/Show.js');
-var calendarModule = require('cloud/Calendar.js');
