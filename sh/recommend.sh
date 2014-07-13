495b63ba94bb816b6bbc1cafea725e2bf568395f
Alfred Chan <a77chan@uwaterloo.ca>
2014-07-13 16:42:07 -0400

    updated recommend example

--- sh/recommend.sh
+++ sh/recommend.sh
@@ -2,5 +2,5 @@ curl -X POST \
   -H "X-Parse-Application-Id: UjD956TYrxLbL58UwNqiSSaHcUBiSp4gs3S0oV5Q" \
   -H "X-Parse-REST-API-Key: 3UnTlreVmT5HBYYX0zKn6sbm9Mlly5F9OGMarrhi" \
   -H "Content-Type: application/json" \
-  -d '{"recommenderID" : "60cYJ8SAgI", "recommendeeID" : "T1e9QEwChA", "showID" : "xIPQTlstw5"}' \
+  -d '{"recommenderID" : "60cYJ8SAgI", "recommendeeID" : "264109563776510", "showID" : "xIPQTlstw5"}' \
   https://api.parse.com/1/functions/recommendShow
